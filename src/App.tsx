466997980c7a29f6fca5219fb5f44740a8e8e83f
unknown <zvonimir.stipanovic1@gmail.com>
2020-10-04 23:14:57 +0200

    enabled persistence

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import MainRouter from './router/MainRouter';
 import { Provider } from 'react-redux';
 import store from './store';
 import Loading from './common/Loading';
+import { firebaseConfig } from './firebase';
+import firebase from 'firebase/app';
 
 function App() {
+    firebaseConfig.firestore().settings({
+        cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED,
+    });
+    firebase.firestore().enablePersistence();
+
     return (
         <Provider store={store}>
             <Loading>
