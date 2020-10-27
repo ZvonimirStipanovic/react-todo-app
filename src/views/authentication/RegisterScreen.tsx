126837e02aacb9db57680dc64a1413d5ac6d4698
zstipanovic <zvonimir@prototyp.digital>
2020-10-27 10:50:15 +0100

    register screen

--- src/views/authentication/RegisterScreen.tsx
+++ src/views/authentication/RegisterScreen.tsx
@@ -1,41 +1,13 @@
 import React, { useState } from 'react';
 import { RouterProps } from 'react-router';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import { Button, Header } from 'components';
+import { Button, Header, TextField } from 'components';
 import { useAuthHook } from 'modules/authentication/hooks';
 import { AppRoute } from 'const';
 import { ButtonSize, ButtonType } from 'models';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        paper: {
-            marginTop: theme.spacing(8),
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        },
-        avatar: {
-            margin: theme.spacing(1),
-            backgroundColor: theme.palette.secondary.main,
-        },
-        form: {
-            width: '100%',
-            marginTop: theme.spacing(1),
-        },
-        submit: {
-            margin: theme.spacing(3, 0, 2),
-        },
-    })
-);
-
 export default function RegisterScreen({ history }: RouterProps) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
-    const classes = useStyles();
 
     const { handleRegister } = useAuthHook();
 
@@ -47,52 +19,33 @@ export default function RegisterScreen({ history }: RouterProps) {
                 to={AppRoute.Home}
                 showRightButtons={false}
             />
-            <Container component="main" maxWidth="xs">
-                <div className={classes.paper}>
-                    <Typography component="h1" variant="h5">
-                        Register
-                    </Typography>
-                    <form className={classes.form}>
-                        <TextField
-                            variant="outlined"
-                            onChange={(
-                                event: React.ChangeEvent<HTMLInputElement>
-                            ) => setEmail(event.target.value)}
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            onChange={(
-                                event: React.ChangeEvent<HTMLInputElement>
-                            ) => setPassword(event.target.value)}
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                        />
-                        <Button
-                            variant={ButtonType.Primary}
-                            size={ButtonSize.Large}
-                            additionalClasses={
-                                'btn--font-med btn--elipsoid btn--shadow-low'
-                            }
-                            handleButtonClick={handleRegisterButton}
-                        >
-                            Register
-                        </Button>
-                    </form>
-                </div>
-            </Container>
+            <div className="v-register">
+                <h1 className="title">Register</h1>
+                <TextField
+                    type="text"
+                    name="email"
+                    placeholder="Email Address"
+                    additionalClasses="textfield--size-lrg textfield--elipsoid "
+                    onChange={setEmail}
+                />
+                <TextField
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    additionalClasses="textfield--size-lrg textfield--elipsoid "
+                    onChange={setPassword}
+                />
+                <Button
+                    variant={ButtonType.Primary}
+                    size={ButtonSize.Large}
+                    additionalClasses={
+                        'btn--font-med btn--elipsoid btn--shadow-low'
+                    }
+                    handleButtonClick={handleRegisterButton}
+                >
+                    Register
+                </Button>
+            </div>
         </div>
     );
 
